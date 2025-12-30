5fd422c275ffa5999928a14a109420e041f9a24c
plabiohju <bimhjuytre@gmail.com>
2025-12-30 09:53:15 -0300

    Add files via upload

---
+++ server1.js
@@ -0,0 +1,90 @@
+import express from "express";
+import cors from "cors";
+import { pool } from "./db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+/* CADASTRAR VINHO */
+app.post("/vinhos", async (req, res) => {
+  const { nome, pais, uva, tamanho, quantidade } = req.body;
+
+  const result = await pool.query(
+    "INSERT INTO vinhos (nome, pais, uva, tamanho, quantidade) VALUES ($1,$2,$3,$4,$5) RETURNING *",
+    [nome, pais, uva, tamanho, quantidade]
+  );
+
+  res.json(result.rows[0]);
+});
+
+/* LISTAR TODOS */
+app.get("/vinhos", async (req, res) => {
+  const result = await pool.query("SELECT * FROM vinhos ORDER BY nome");
+  res.json(result.rows);
+});
+
+/* BUSCAR */
+/* BUSCA AVANÇADA */
+app.get("/vinhos/busca", async (req, res) => {
+  const {
+    nome = "",
+    pais = "",
+    uva = "",
+    tamanho = "",
+    quantidade = ""
+  } = req.query;
+
+  const r = await pool.query(
+    `SELECT * FROM vinhos
+     WHERE nome ILIKE $1
+       AND pais ILIKE $2
+       AND uva ILIKE $3
+       AND CAST(tamanho AS TEXT) ILIKE $4
+       AND CAST(quantidade AS TEXT) ILIKE $5
+     ORDER BY id`,
+    [
+      `%${nome}%`,
+      `%${pais}%`,
+      `%${uva}%`,
+      `%${tamanho}%`,
+      `%${quantidade}%`
+    ]
+  );
+
+  res.json(r.rows);
+});
+
+
+/* ZERADOS */
+app.get("/vinhos/zerados", async (req, res) => {
+  const result = await pool.query(
+    "SELECT * FROM vinhos WHERE quantidade = 0"
+  );
+  res.json(result.rows);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
+/*editar*/
+app.put("/vinhos/:id", async (req, res) => {
+  const { nome, pais, uva, tamanho, quantidade } = req.body;
+
+  await pool.query(
+    `UPDATE vinhos 
+     SET nome=$1, pais=$2, uva=$3, tamanho=$4, quantidade=$5 
+     WHERE id=$6`,
+    [nome, pais, uva, tamanho, quantidade, req.params.id]
+  );
+
+  res.sendStatus(200);
+});
+/*excluir*/
+app.delete("/vinhos/:id", async (req, res) => {
+  await pool.query("DELETE FROM vinhos WHERE id=$1", [req.params.id]);
+  res.sendStatus(200);
+});
+
