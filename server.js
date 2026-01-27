4b36cbc731b6c1d2bcab7fce5b96c661cdf258bb
plabiohju <bimhjuytre@gmail.com>
2026-01-27 17:52:02 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.json());
 
 /* CADASTRAR VINHO */
 app.post("/vinhos", async (req, res) => {
-  const { nome, pais, uva, tamanho, quantidade,data, } = req.body;
+  const { nome, pais, uva, tamanho, quantidade,data,prato,Qtd, } = req.body;
 
   const result = await pool.query(
-    "INSERT INTO vinhos (nome, pais, uva, tamanho, quantidade, data) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",
-    [nome, pais, uva, tamanho, quantidade,data]
+    "INSERT INTO vinhos (nome, pais, uva, tamanho, quantidade, data,prato,Qtd) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
+    [nome, pais, uva, tamanho, quantidade,data,prato,Qtd]
   );
 
   res.json(result.rows[0]);
@@ -33,7 +33,9 @@ app.get("/vinhos/busca", async (req, res) => {
     uva = "",
     tamanho = "",
     quantidade = "",
-    data = ""
+    data = "",
+    prato = "",
+    Qtd = ""
   } = req.query;
 
   const r = await pool.query(
@@ -44,6 +46,8 @@ app.get("/vinhos/busca", async (req, res) => {
        AND CAST(tamanho AS TEXT) ILIKE $4
        AND CAST(quantidade AS TEXT) ILIKE $5
        AND CAST(data AS TEXT) ILIKE $6
+       AND CAST(prato AS TEXT) ILIKE $7
+       AND CAST(Qtd AS TEXT) ILIKE $8
      ORDER BY id`,
     [
       `%${nome}%`,
@@ -51,7 +55,9 @@ app.get("/vinhos/busca", async (req, res) => {
       `%${uva}%`,
       `%${tamanho}%`,
       `%${quantidade}%`,
-      `%${data}%`
+      `%${data}%`,
+      `%${prato}%`,
+      `%${Qtd}%`
     ]
   );
 
@@ -74,7 +80,7 @@ app.listen(PORT, () => {
 });
 /*editar*/
 app.put("/vinhos/:id", async (req, res) => {
-  const { nome, pais, uva, tamanho, quantidade, data } = req.body;
+  const { nome, pais, uva, tamanho, quantidade, data,prato,Qtd } = req.body;
 
   await pool.query(
     `
@@ -85,9 +91,11 @@ app.put("/vinhos/:id", async (req, res) => {
       tamanho = COALESCE($4, tamanho),
       quantidade = COALESCE($5, quantidade),
       data = COALESCE($6, data)
-    WHERE id = $7
+      prato = COALESCE($7, prato)
+      Qtd = COALESCE($8, Qtd)
+    WHERE id = $9
     `,
-    [nome, pais, uva, tamanho, quantidade, data, req.params.id]
+    [nome, pais, uva, tamanho, quantidade, data,prato,Qtd, req.params.id]
   );
 
   res.sendStatus(200);
